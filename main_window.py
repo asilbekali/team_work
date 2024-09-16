edc4128366e07c1cb0ab8110932df30125de1260
Alisher Sharipov <alishersharipov670@gmail.com>
2024-09-16 13:04:38 +0500

    yangilanid

--- main_window.py
+++ main_window.py
@@ -1,7 +1,11 @@
 from PyQt5.QtWidgets import *
-from mypost import MyPost
-import mysql.connector
 from team_project import *
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QSize
+import mysql.connector
+from mypost import MyPost
+from color_back import GradientWidget
+
 
 
 mydb = mysql.connector.connect(
@@ -14,29 +18,63 @@ mydb = mysql.connector.connect(
 class MyWindow(QWidget):
     def __init__(self,text):
         super().__init__()
+        self.setFixedSize(1700, 900)
+        self.gradient_widget = GradientWidget(self)
+        self.gradient_widget.setGeometry(0, 0, 0, 0)
+        self.gradient_widget.setStyleSheet("background: transparent;")
+
         self.user_nomer=text
         self.v_main_lay = QVBoxLayout()
         self.h_btn_lay = QHBoxLayout()
-        
-        self.write_post_btn = QPushButton("Post yozish", clicked=self.createpost)
-        self.mypost_btn = QPushButton("Postlarim", clicked=self.mypost)
-        self.qlist = QListWidget()
+        self.h_btn1_lay = QHBoxLayout()
+        self.h_qlist_lay = QHBoxLayout()
 
-        self.h_btn_lay.addWidget(self.write_post_btn)
+        self.label = QLabel("My posts")
+        self.label.setStyleSheet("""
+            font-family: 'Comic Sans MS';
+            font-size: 50px;
+            font-weight: bold;
+            color: white;
+        """)
+        
+        self.write_post_btn = QPushButton("", clicked=self.createpost)
+        self.write_post_btn.setIcon(QIcon("pen.png"))
+        self.write_post_btn.setIconSize(QSize(50,50))
+        self.write_post_btn.setStyleSheet("border-radius:10px")
+
+        self.mypost_btn = QPushButton("", clicked=self.mypost)
+        self.mypost_btn.setIcon(QIcon("man.png"))
+        self.mypost_btn.setIconSize(QSize(50,50))
+        self.mypost_btn.setStyleSheet("border-radius:10px")
+
+        self.qlist = QListWidget()
+        self.qlist.setFixedSize(1000,700)
+        
+        self.h_qlist_lay.addWidget(self.qlist)
+
+        self.h_btn_lay.addStretch()
+        self.h_btn_lay.addWidget(self.label)
+        self.h_btn_lay.addStretch()
         self.h_btn_lay.addWidget(self.mypost_btn)
 
+        self.h_btn1_lay.addStretch()
+        self.h_btn1_lay.addWidget(self.write_post_btn)
+
         self.v_main_lay.addLayout(self.h_btn_lay)
-        self.v_main_lay.addWidget(self.qlist)
+        self.v_main_lay.addLayout(self.h_qlist_lay)
+        self.v_main_lay.addLayout(self.h_btn1_lay)
         self.print_post()
 
         self.setLayout(self.v_main_lay)
 
     def print_post(self):
             cursor = mydb.cursor()
-            cursor.execute("SELECT word from post inner join users on post.id = users.post_id")
+            cursor.execute("SELECT word , data from post inner join users on post.id = users.post_id")
             posts = cursor.fetchall()
             for post in posts:
+                print(post)
                 self.qlist.addItem(post[0])
+                self.qlist.addItem(str(post[1]))
 
 
     def createpost(self):
@@ -49,7 +87,7 @@ class MyWindow(QWidget):
 
 if __name__ == "__main__":
     app = QApplication([])
-    win = MyWindow()
+    win = MyWindow("salom")
     win.show()
     app.exec_()
 
